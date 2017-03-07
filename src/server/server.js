03995b436a3b1e689b729531030031e738837b10
matias <matiaskochman@gmail.com>
2017-03-07 15:59:35 -0300

    installed gulp and setup the tasks to compile, installed jasmine and setup tests, created server.js and settings.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,36 @@
-const hh = 'hola';
-console.log('${hh} Matias.');
+import express from 'express';
+import http from 'http';
 
-const obj = {hey:1}:
-const obj2 = {...obj,bleg:2};
+//using brackets because isDevelopement is not exported as default.
+import {isDevelopement} from './settings';
 
-console.log(obj2);
+//----------------
+// Setup
+
+const app = express();
+const server = new http.Server(app);
+
+//----------------
+// Configuration
+
+app.set("view engine","pug");
+app.use(express.static("public")); //tell express to serve out any file in the public folder. Example: favicon.ico
+
+const useExternalStyles = !isDevelopement;
+const scriptRoot = isDevelopement
+    ? "http://localhost:8080/build"
+    : "/build";
+
+app.get("*",(req,res) => {
+  res.render("index",{
+    useExternalStyles,
+    scriptRoot
+  })
+});
+
+//----------------
+// Startup
+const port = process.env.PORT || 3000;
+server.listen(port,()=>{
+  console.log(`Started http server on port ${port}`);
+})
